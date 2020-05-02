bdda2bf26189c9648da811da34b6b6cb70ddadef
satoken722 <sato.kenta@isee.nagoya-u.ac.jp>
2020-05-02 23:38:29 +0900

    fitst

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject ClearUI;
+    public GameObject GameOverUI;
+
+    public void Update()
+    {
+        int count = GameObject.FindGameObjectsWithTag("Block").Length;
+        int count1 = GameObject.FindGameObjectsWithTag("Player").Length;
+
+        if (count == 0)
+        {
+            // ClearUIをアクティブにする
+            ClearUI.SetActive(true);
+            // クリア時にBallを消滅する
+            GameObject obj = GameObject.Find("Ball");
+            Destroy(obj);
+        }
+        // Ballが無くなり、かつBlockが残っている時にゲームオーバーとなる条件
+        if (count1 == 0 && count != 0)
+        {
+            //GameOverUIをアクティブにする
+            GameOverUI.SetActive(true);
+        }
+    }
+}
